3c93a9112b3ed7ab591d6c4b097a92b39a6e79bf
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2021-04-29 13:47:33 +0100

    site skeleton done

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+const { posts } = require('./testPosts');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use(helmet());
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {posts: posts});
 })
 
 app.listen(process.env.PORT || 5000, () => {
