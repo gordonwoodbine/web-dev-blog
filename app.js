d8014660cadb0bc317fb76dc4f27da1a6f1c51d9
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2021-04-29 15:19:33 +0100

    finished basic compose page and styles

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.use(helmet());
 
 app.get('/', (req, res) => {
   res.render('index', {posts: posts});
+});
+
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', (req, res) => {
+  const newPost = {
+    title: req.body.title,
+    body: req.body.body,
+    postDate: new Date().toLocaleString()
+  }
+
+  posts.push(newPost);
+  res.redirect('/');
 })
 
 app.listen(process.env.PORT || 5000, () => {
