da57e5bf01e68452bb9e260a60951b9de9757c33
gordonwoodbine <gordon_woodbine@yahoo.co.uk>
2021-04-29 11:12:41 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server running...');
+});
